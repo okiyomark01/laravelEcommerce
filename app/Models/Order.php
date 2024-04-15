7478ea62ce8c13c82f112d2af5947b727eaf1230
okiyomark01 <okiyomark01@gmail.com>
2024-04-15 11:59:22 +0300

    Category and Brand resources completed

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'grand_total',
+        'payment_method',
+        'payment_status',
+        'status',
+        'currency',
+        'shipping_amount',
+        'shipping_method',
+        'notes',
+    ];
+
+    //creating table relationships ie 1 order belongs to 1 user
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+
+    public function address()
+    {
+        return $this->hasOne(Address::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,39 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'brand_id',
+        'name',
+        'slug',
+        'images',
+        'description',
+        'price',
+        'is_active',
+        'is_feature',
+        'in_stock',
+        'on_sale',
+    ];
+
+    protected $casts = [
+        'images' => 'array',
+    ];
+
+    //creating relationships
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
 }
